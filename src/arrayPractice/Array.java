9b7617b675dec3cbfb29c27b6e0854b3c32fb619
saiful8183 <Manha103019@gmail.com>
2023-01-11 01:38:03 -0500

    My java project

---
+++ src/arrayPractice/Array.java
@@ -0,0 +1,69 @@
+package arrayPractice;
+
+public class Array {
+
+	public static void main(String[] args) {
+//
+//int[]myArray=new int[4];{
+//	
+//	myArray[0]=56;
+//	myArray[1]=57;
+//	myArray[2]=58;
+//	myArray[3]=569;
+//
+//	
+//	System.out.println(myArray[0]);
+//	System.out.println(myArray[1]);
+//	System.out.println(myArray[2]);
+//	System.out.println(myArray[3]);
+//	//System.out.println(myArray[4]);
+//
+//	
+//	
+//	int size=myArray.length;
+//	System.out.println("The length of the size is:"+size);
+//	System.out.println(myArray.length);
+//	
+//}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	int a[]=new int[3];  //Declaration and instantiation
+		
+		a[0]=10;   //Initialization
+		a[1]=50;
+		a[2]=30;
+//		
+//System.out.println(a[0]);
+//System.out.println(a[1]);
+//System.out.println(a[2]);
+//		int size=a.length;
+//	System.out.println("The length of array is:"+size);	
+//		
+//	System.out.println(a.length);	
+//		
+		
+	for(int i=0;i<a.length;i++)//length is the property of array  
+		System.out.println(a[i]); 
+		
+		
+		
+		
+		
+}
+
+}
+	
+
+
